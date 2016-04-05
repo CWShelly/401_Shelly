fc4865ce4c0743f80c02f5fc37392c595c56579e
Shelly Yusuf <SputnikSeattle@Michelles-MacBook-Air.local>
2016-04-05 12:36:30 -0700

    added updated files to include a hardcoded option as well as a process.argv[2] option

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 var greet = require(__dirname + '/lib/greet');
 var greetPerson = greet(process.argv[2]);
+var greetPerson2 = greet('shelly');
+
 console.log(greetPerson);
+console.log(greetPerson2);
--- test/greet_test.js
+++ test/greet_test.js
@@ -1,10 +1,16 @@
 const chai = require('chai');
 const expect = chai.expect;
 const greet = require(__dirname + '/../lib/greet');
-const name = process.argv[2];
+
+const name = 'Shelly';
 
 describe('the greeting', ()=>{
   it('should greet the person by the name given in the CLI', ()=>{
     expect(greet(name)).to.equal('hello ' + name);
   });
+
+  it('should greet the person by the name hardcoded', ()=>{
+    expect(greet('Shelly')).to.equal('hello Shelly');
+  });
+
 });
