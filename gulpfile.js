5f7e005abd83f3c24e7f5d09cac3a0696373b076
Shelly Yusuf <SputnikSeattle@Michelles-MacBook-Air.local>
2016-04-05 21:38:46 -0700

    added gulpfile.js and package.json

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+const mocha = require('gulp-mocha');
+const gutil = require('gulp-util');
+
+var files =['index.js', 'lib/**/*.js', 'test/**/*.js','gulpfile.js'];
+
+gulp.task('mocha', function(){
+  return gulp.src(['./test/**/*test.js'], { read : false })
+  .pipe(mocha({
+    reporter:'landing'
+  }))
+  .on('error', gutil.log);
+});
+
+gulp.task('lint:test', function(){
+  return gulp.src('lint:test', ()=> {
+    return gulp.src('./test/**/*test.js')
+    .pipe(eslint({
+      rules:{
+        'indent':['error', 2]
+      },
+      envs:[
+        'mocha'
+      ]
+    }));
+  });
+});
+
+gulp.task('lint:nontest', ()=>{
+  return gulp.src(files)
+  .pipe(eslint({
+    rules:{
+      'indent':['error', 2]
+    }
+  }))
+  .pipe(eslint.format());
+});
+
+
+
+gulp.task('watch-files', function(){
+  gulp.watch(['lib/**', 'test/**'],['mocha']);
+  gulp.watch(['lib/**', 'test/**'],['lint:test']);
+  gulp.watch(['lib/**', 'test/**'],['lint:nontest']);
+});
+
+
+gulp.task('index', ['watch-files']);
+gulp.task('default', ['index']);
+//
+// gulp.task('lint:test', ()=>{
+//   return gulp.src('./test/**/*test.js')
+//   .pipe(eslint({
+//     rules:{
+//       'indent': ['error', 2]
+//     },
+//     envs:[
+//       'mocha'
+//     ]
+//   }))
+//   .pipe(eslint.format());
+// });
+//
+// gulp.task('lint:nontest', ()=>{
+//   return gulp.src(files)
+//   .pipe(eslint({
+//     rules:{
+//       'indent': ['error', 2]
+//     }
+//   }))
+//   .pipe(eslint.format());
+// });
+//
+// gulp.task('lint', ['lint:nontest', 'lint:test']);
+// gulp.task('default', ['lint']);
--- test/greet_test.js
+++ test/greet_test.js
@@ -1,15 +1,14 @@
 const chai = require('chai');
 const expect = chai.expect;
 const greet = require(__dirname + '/../lib/greet');
-
 const name = 'Shelly';
 
 describe('the greeting', ()=>{
-  it('should greet the person by the name given in the CLI', ()=>{
+  it('needs greet the person by the name given in the CLI', ()=>{
     expect(greet(name)).to.equal('hello ' + name);
   });
 
-  it('should greet the person by the name hardcoded', ()=>{
+  it('needs greet the person by the name hardcoded', ()=>{
     expect(greet('Shelly')).to.equal('hello Shelly');
   });
 
